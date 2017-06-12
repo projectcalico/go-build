0c751dcaac6bd72bee6f62a8aa16b0b631b13d9f
ligc <liguangc@cn.ibm.com>
2017-06-12 08:38:32 -0400

    add set -e to create_dockerfile.ppc64le.sh, make changes to a temp file and then move it to Dockerfile.ppc64le

--- create_dockerfile.ppc64le.sh
+++ create_dockerfile.ppc64le.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -e
+
 # Create Dockerfile.ppc64le based on Dockerfile
 
 # Use the ppc64le/golang base image
-sed 's/FROM golang:.*/FROM ppc64le\/golang:1.8.1-alpine/' Dockerfile > Dockerfile.ppc64le
+sed 's/FROM golang:.*/FROM ppc64le\/golang:1.8.1-alpine/' Dockerfile > tmp.Dockerfile.ppc64le
 
 # ppc64le platform does not need the syscall patch for x86
-sed -i '/^COPY patches/,/RUN go install -v -a syscall/d' Dockerfile.ppc64le
+sed -i '/^COPY patches/,/RUN go install -v -a syscall/d' tmp.Dockerfile.ppc64le
+
+mv -f tmp.Dockerfile.ppc64le Dockerfile.ppc64le

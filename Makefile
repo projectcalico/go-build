37fb52ff79bdfd8283a8b949135e827cc5457e18
David Wilder <wilder@us.ibm.com>
2017-07-25 16:26:38 -0700

    Switch ppc64le build environment to ppc64le/golang
    The Go compiler on ppc64le/golang:1.8.1-alpine is built to generate PIEs
    by default (buildmode=pie). Unfortunately this is inconsistent with x86
    Alpine that defaulting to buildmode=exe.  Therefor I am switching the
    ppc64le build environment to ppc64le/golang:1.8.3. This environment has
    proven to be stable on an internal ppc64le Calico port.
    
    Signed-off-by: David Wilder <wilder@us.ibm.com>

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ calico/go-build:
 	docker build -t calico/go-build .
 
 calico/go-build-ppc64le:
-	docker build -t calico/go-build-ppc64le -f Dockerfile.ppc64le .
+	docker build -t calico/go-build-ppc64le -f Dockerfile-ppc64le .
---
+++ entrypoint-ppc64le.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID" 1>&2
+useradd  -s /bin/bash -u $USER_ID --create-home  user
+export HOME=/home/user
+
+if [ -n "$EXTRA_GROUP_ID"  ]; then
+  echo "Adding user to additional GID : $EXTRA_GROUP_ID" 1>&2
+  # Adding the group can fail if it already exists.
+  if addgroup -g $EXTRA_GROUP_ID group; then
+    addgroup user group
+  else
+    echo "Adding user to existing group instead" 1>&2
+    addgroup user `getent group $EXTRA_GROUP_ID | cut -d: -f1`
+  fi
+fi  
+
+exec /sbin/su-exec user "$@"
